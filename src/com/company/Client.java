81a730b26d498a497fe34a9c1d1edc8d85064fa7
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-01-29 13:59:43 +0200

    Client Setup Commit

--- src/com/company/Client.java
+++ src/com/company/Client.java
@@ -1,4 +1,39 @@
 package com.company;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+    public static void main(String[] args) {
+        try {
+            // Socket with localhost and the same port in server ...
+            Socket socket = new Socket("localhost", 2023);
+
+            // DataInputStream to Read Data from Server ...
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+
+            // DataOutputStream to Write Data to Server ...
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            // Accept Ready Message from Server :
+            System.out.println(dataInputStream.readUTF());
+
+            // Write Message and send it to server ...
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Please Enter Your Message : ");
+            String clientMess = scanner.nextLine();
+            dataOutputStream.writeUTF(clientMess);
+
+            // Close our Resources ...
+            dataOutputStream.close();
+            dataInputStream.close();
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
